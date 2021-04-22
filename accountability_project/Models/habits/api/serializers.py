5c420ed007a8c720491976dae5193ab97a5bb324
thiago1623 <thiago.96t16@gmail.com>
2021-04-21 21:09:25 -0500

    [UPDATE]:--delete

--- accountability_project/Models/habits/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from apps.habits.models import Habit
-
-
-class HabitSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Habit
-        fields = '__all__'
--- accountability_project/Models/scoreboards/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from apps.scoreboards.models import ScoreBoard
-
-
-class ScoreBoardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ScoreBoard
-        fields = '__all__'
