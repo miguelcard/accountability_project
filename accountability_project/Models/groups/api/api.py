1651486d0fcd36a8ecdaf3b9ab7f7e3522278ce5
Miguel Cardenas <migueluy360@hotmail.com>
2021-06-25 08:54:49 +0200

    code cleanded up, only generic class was left

--- accountability_project/Models/groups/api/api.py
+++ accountability_project/Models/groups/api/api.py
@@ -31,45 +31,5 @@ class GroupGenericApiView(generics.GenericAPIView,
     def delete(self, pk):
         return self.destroy(request, pk)
 
-    
 
-class GroupApiView(APIView):
-
-    def get(self, request):
-        groups = Group.objects.all()
-        groups_serializer = GroupSerializer(groups, many=True)
-        return Response(groups_serializer.data, status= status.HTTP_200_OK)
-
-    def post(self, request):
-        groups_serializer = GroupSerializer(data = request.data)
-        if groups_serializer.is_valid():
-            groups_serializer.save()
-            return Response(groups_serializer.data, status= status.HTTP_201_CREATED)
-        return Response(groups_serializer.errors, status= status.HTTP_400_BAD_REQUEST)
-
-class GroupDetailApiView(APIView):
-
-    # method to just get  group object and check if its valid
-    def get_object(self, pk):
-        try:
-            return Group.objects.get(id = pk)
-        except Group.DoesNotExist:
-            return Response({'Message':'group not found'}, status= status.HTTP_404_NOT_FOUND)
-
-    def get(self, request, pk):
-        group = self.get_object(pk)
-        group_serializer = GroupSerializer(group)
-        return Response(group_serializer.data, status= status.HTTP_200_OK)
-
-    def put(self, request, pk):
-        group = self.get_object(pk)
-        group_serializer = GroupSerializer(group, data = request.data)
-        if group_serializer.is_valid():
-            group_serializer.save()
-            return Response(group_serializer.data, status= status.HTTP_200_OK)
-        return Response({'message': 'Object fields not allowed'}, status= status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        group = self.get_object(pk)
-        group.delete()
-        return Response({'Message':'group has been deleted'}, status= status.HTTP_204_NO_CONTENT)
+# use perform methods to add custom logic, https://www.django-rest-framework.org/api-guide/generic-views/#mixins
--- accountability_project/Models/groups/api/urls.py
+++ accountability_project/Models/groups/api/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from Models.groups.api.api import GroupApiView, GroupDetailApiView, GroupGenericApiView
 
 urlpatterns = [
-    path('group/', GroupApiView.as_view()),
-    path('group/<int:pk>', GroupDetailApiView.as_view()),
-    path('groupgeneric/',GroupGenericApiView.as_view()),
-    path('groupgeneric/<int:pk>',GroupGenericApiView.as_view())
+    path('group/',GroupGenericApiView.as_view()),
+    path('group/<int:pk>',GroupGenericApiView.as_view())
 ]
