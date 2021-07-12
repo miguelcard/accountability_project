2073e9c37887f149c7d8b2624f789d835f758a30
thiago1623 <thiago.96t16@gmail.com>
2021-07-11 20:06:42 -0500

    [UPDATE]:-- add new changes , in the register and login send same data sended to the front of app

--- accountability_project/Models/users/views.py
+++ accountability_project/Models/users/views.py
@@ -18,7 +18,7 @@ class RegisterAPI(generics.GenericAPIView):
         user = serializer.save()
         return Response({
         "user": UserSerializer(user, context=self.get_serializer_context()).data,
-        "token": AuthToken.objects.create(user)[1]
+        "authentication": {"token": AuthToken.objects.create(user)[1]}
         })
 
 #Login API 
