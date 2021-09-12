64a159ad5f6882ca22f432764f55418e92e3d369
Miguel Cardenas <migueluy360@hotmail.com>
2021-09-12 16:28:30 +0200

    added models to admin GUI

--- accountability_project/Models/habits/admin.py
+++ accountability_project/Models/habits/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from Models.habits.models import RecurrentHabit, Goal
+from Models.habits.models import RecurrentHabit, Goal, CheckMark, Milestone
 
 # Register your models here.
 admin.site.register(RecurrentHabit)
 admin.site.register(Goal)
+admin.site.register(CheckMark)
+admin.site.register(Milestone)
 
