2f30f366f1135f23376886e1b7ef7ae1503c4959
Miguel Cardenas <migueluy360@hotmail.com>
2021-10-22 08:45:38 +0200

    organized code, nested checkmark serializer added to recurrent habits too

--- accountability_project/Models/habits/api/serializers.py
+++ accountability_project/Models/habits/api/serializers.py
@@ -5,7 +5,33 @@ from Models.habits.models import Goal, RecurrentHabit, HabitTag, BaseHabit, Chec
 from rest_framework import serializers
 import datetime
 
-# Firstly the serializers used as "Nested Serializers" for the Habits Serializers
+# Filters the Checkmarks by Date, by default only the ones in the last 7 days are shown
+class FilteredListSerializer(serializers.ListSerializer):
+
+    def to_representation(self, data):
+        checkmarks_from = self.context['request'].GET.get('checkmarks_from', None)
+        checkmarks_to = self.context['request'].GET.get('checkmarks_to', None)
+        
+        if (checkmarks_from != None and checkmarks_to != None):
+            data = data.filter(date__range=[checkmarks_from, checkmarks_to])
+        elif(checkmarks_from !=None):
+            data = data.filter(date__gt=checkmarks_from) 
+        elif(checkmarks_to != None):
+            checkmarks_to_date = datetime.datetime.strptime(checkmarks_to, '%Y-%m-%d')
+            last_week = checkmarks_to_date - datetime.timedelta(days = 7)
+            data = data.filter(date__range=[last_week, checkmarks_to])
+        else:
+            today = datetime.date.today()
+            last_week = datetime.date.today() - datetime.timedelta(days = 7)
+            data = data.filter(date__range=[last_week, today])
+
+        return super(FilteredListSerializer, self).to_representation(data)
+
+class CheckMarkNestedSerializer(serializers.ModelSerializer):
+    class Meta:
+        list_serializer_class = FilteredListSerializer
+        model = CheckMark
+        fields = '__all__'
 
 class HabitTagSerializer(serializers.ModelSerializer):
     class Meta:
@@ -26,6 +52,8 @@ class RecurrentHabitSerializerToWrite(serializers.ModelSerializer):
 
 class RecurrentHabitSerializerToRead(serializers.ModelSerializer):
     tags = HabitTagSerializer(many=True, read_only=True)
+    checkmarks = CheckMarkNestedSerializer(many=True, read_only=True)
+
     class Meta:
         model = RecurrentHabit
         fields = '__all__'
@@ -45,41 +73,6 @@ class GoalSerializerToWrite(serializers.ModelSerializer):
         serializer = GoalSerializerToRead(instance)
         return serializer.data
 
-# Place at the beggining        
-# class CheckMarkSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = CheckMark
-#         fields = '__all__'
-
-# Filters the Checkmarks by Date, by default only the ones in the last 7 days are shown
-class FilteredListSerializer(serializers.ListSerializer):
-
-    def to_representation(self, data):
-
-        checkmarks_from = self.context['request'].GET.get('checkmarks_from', None)
-        checkmarks_to = self.context['request'].GET.get('checkmarks_to', None)
-        
-        if (checkmarks_from != None and checkmarks_to != None):
-            data = data.filter(date__range=[checkmarks_from, checkmarks_to])
-        elif(checkmarks_from !=None):
-            data = data.filter(date__gt=checkmarks_from) 
-        elif(checkmarks_to != None):
-            checkmarks_to_date = datetime.datetime.strptime(checkmarks_to, '%Y-%m-%d')
-            last_week = checkmarks_to_date - datetime.timedelta(days = 7)
-            data = data.filter(date__range=[last_week, checkmarks_to])
-        else:
-            today = datetime.date.today()
-            last_week = datetime.date.today() - datetime.timedelta(days = 7)
-            data = data.filter(date__range=[last_week, today])
-
-        return super(FilteredListSerializer, self).to_representation(data)
-
-class CheckMarkNestedSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        list_serializer_class = FilteredListSerializer
-        model = CheckMark
-        fields = '__all__'
 
 class GoalSerializerToRead(serializers.ModelSerializer):
     tags = HabitTagSerializer(many=True, read_only=True)
