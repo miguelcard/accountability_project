b795d7d1e4759360d01aa1e14068be516481cf38
Miguel Cardenas <migueluy360@hotmail.com>
2021-11-16 08:55:51 +0100

    commented out unused code

--- accountability_project/Models/habits/api/permissions.py
+++ accountability_project/Models/habits/api/permissions.py
@@ -3,18 +3,6 @@ from Models.habits.models import BaseHabit
 from Models.habits.models import CheckMark
 
 # Note: has_permission is called on all HTTP requests whereas, has_object_permission is called only for detail methods (GET, PUT, PATCH) so it can be that both are called
-class NameOfPermission(BasePermission):
-    message = 'You must be the owner of this object'
-
-    def has_permission(self, request, view):     
-        # no object specific checking       # A more generic permission (not per object)
-        pass
-    
-    def has_object_permission(self, request, view, obj):
-        my_safe_methods = ['PUT']
-        if request.method in my_safe_methods:
-            return True
-        return obj.user == request.user
         
 # ADD -> if in safe methods AND if in same space ...(he can view) -> add another permission for this
 class IsOwnerOfParentHabit(BasePermission):
@@ -32,3 +20,17 @@ class IsOwnerOfParentHabit(BasePermission):
     def has_object_permission(self, request, view, obj):
         return (obj.habit.owner == request.user)
 
+# Example: 
+# class NameOfPermission(BasePermission):
+    
+#     message = 'You must be the owner of this object'
+
+#     def has_permission(self, request, view):     
+#         # no object specific checking       # A more generic permission (not per object)
+#         pass
+    
+#     def has_object_permission(self, request, view, obj):
+#         my_safe_methods = ['PUT']
+#         if request.method in my_safe_methods:
+#             return True
+#         return obj.user == request.user
