b2ece35e95bf2eb47f386676f898206419e51450
Miguel Cardenas <migueluy360@hotmail.com>
2022-04-03 19:26:15 +0200

    created get endpoint for spaceroles of an space, see, https://app.clickup.com/t/2a1dxqj

--- accountability_project/Models/spaces/api/pagination.py
+++ accountability_project/Models/spaces/api/pagination.py
@@ -3,11 +3,16 @@ from rest_framework.pagination import PageNumberPagination
 """ ---------Pagination classes fot Spaces-------"""
 
 class SpacesPagination(PageNumberPagination):
-    page_size = 9
+    page_size = 10
     page_size_query_param = 'page_size'
-    max_page_size = 50
+    max_page_size = 100
 
 class SpaceHabitsPagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
-    max_page_size = 35 
+    max_page_size = 100
+
+class SpaceRolesPagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 900
--- accountability_project/Models/spaces/api/urls.py
+++ accountability_project/Models/spaces/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from Models.spaces.api.api import (SpacesApiView, SpacesDetailApiView, SpacesDetailRetrieveWithHabitsApiView, SpaceHabitsApiView, 
-SpaceRoleDeleteApiView, SpaceRoleInviteApiView, SpaceRoleEditApiView)
+SpaceRoleDeleteApiView, SpaceRoleInviteApiView, SpaceRoleEditApiView, SpaceRolesListApiView)
 
 urlpatterns = [
     path('v1/spaces/', SpacesApiView.as_view(), name='spaces'),
     path('v1/spaces/<int:pk>', SpacesDetailRetrieveWithHabitsApiView.as_view(), name='spaces-detail-habits'),
     path('v1/spaces/<int:pk>/simple/', SpacesDetailApiView.as_view(), name='spaces-detail'),
     path('v1/spaces/<int:space_pk>/habits/', SpaceHabitsApiView.as_view(), name='spaces-detail-habits'),
+    path('v1/spaces/<int:space_pk>/spaceroles/', SpaceRolesListApiView.as_view(), name='spaceroles-list'),
     path('v1/spaceroles/delete/<int:pk>', SpaceRoleDeleteApiView.as_view(), name='spacerole-delete'),
     path('v1/spaceroles/invite/', SpaceRoleInviteApiView.as_view(), name='spacerole-invite'),
     path('v1/spaceroles/manage/<int:pk>', SpaceRoleEditApiView.as_view(), name='spacerole-edit-delete'),
